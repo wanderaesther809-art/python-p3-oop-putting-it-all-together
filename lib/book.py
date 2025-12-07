7844c4672775a79d3c489895ba14cbd5cc7b8260
Esther Wandera <wanderaesther809@gmail.com>
2025-12-08 00:23:39 +0300

    initial commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,32 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(self, title="Unknown", page_count=0):
+        self.title = title
+        self.page_count = page_count
+
+    # Title property
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if isinstance(value, str):
+            self._title = value.title()
+        else:
+            raise ValueError("Title must be a string")
+
+    # Page count property
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    # Method to turn page
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,30 @@
-#!/usr/bin/env python3
+APPROVED_SIZES = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
 class Shoe:
-    pass
+    def __init__(self, brand="Adidas", size=9):
+        self.brand = brand
+        self.size = size
+        self.condition = "New"
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        self._brand = value
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if value in APPROVED_SIZES:
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
